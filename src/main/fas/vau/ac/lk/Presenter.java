817ef42c0af9893002833037012aa3fdd9c92ffb
aathif12 <aathifahamad4@gmail.com>
2025-04-25 23:41:47 +0530

    GEnrate getter and Sttters for Presenter

--- src/main/fas/vau/ac/lk/Presenter.java
+++ src/main/fas/vau/ac/lk/Presenter.java
@@ -16,7 +16,39 @@ public class presenter extends Person {
 	private String country;
 	private String job_title;
 	
-	@ManyToMany
+	public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    @ManyToMany
 	@JoinTable(name="presenter_sessions",
 	           joinColumns = @JoinColumn(name="presenters_id"),
 	           inverseJoinColumns  = @JoinColumn(name="sessions_id"))
