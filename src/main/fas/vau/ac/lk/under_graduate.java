5e7cc043ad004d7ce230b246c53271f5b3551633
aathif12 <aathifahamad4@gmail.com>
2025-04-25 23:29:14 +0530

    Add Undergraduate details

---
+++ src/main/fas/vau/ac/lk/under_graduate.java
@@ -0,0 +1,23 @@
+package fas.vau.ac.lk;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class under_graduate extends Person{
+    @Id
+	private String id;
+	private String degree;
+	private String university;
+	
+	@ManyToMany
+	@JoinTable(name="under_graduate_sessions ",
+	           joinColumns = @JoinColumn(name="under_graduates_id"),
+	           inverseJoinColumns = @JoinColumn(name="sessions_id"))
+	private List<session> sessions;
+}
